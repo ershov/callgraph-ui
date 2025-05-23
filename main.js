251916c8a14292278b7656e98272ade4e9689a81
Yury Ershov <yuriy.ershov@gmail.com>
2025-05-23 11:02:37 +1000

    Add icon

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 900,
+    icon: path.join(__dirname, 'assets/icon.png'),
     webPreferences: {
       // Security settings
       nodeIntegration: true,
